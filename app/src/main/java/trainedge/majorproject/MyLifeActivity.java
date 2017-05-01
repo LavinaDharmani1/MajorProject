70914f9628bf2861f151ebc4d22ab4efbc799b64
LavinaDharmani1 <lavina.dharmani@gmail.com>
2017-05-01 23:52:04 +0530

    progress
    progress bar in mylife

--- app/src/main/java/trainedge/majorproject/MyLifeActivity.java
+++ app/src/main/java/trainedge/majorproject/MyLifeActivity.java
@@ -1,5 +1,6 @@
 package trainedge.majorproject;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -10,15 +11,18 @@ import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class MyLifeActivity extends AppCompatActivity {
 
-    private WebView wvMyLife;
+     WebView wvMyLife;
+    ProgressBar bar;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_my_life);
+        bar =(ProgressBar) findViewById(R.id.progressBar2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -30,9 +34,32 @@ public class MyLifeActivity extends AppCompatActivity {
         wvMyLife.loadUrl("https://www.mylife.com");
     }
     public class MyLifeCallback extends WebViewClient {
+
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            return false;
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            bar.setVisibility(View.GONE);
         }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode==KeyEvent.KEYCODE_BACK) && wvMyLife.canGoBack()){
+            wvMyLife.goBack();
+                return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
