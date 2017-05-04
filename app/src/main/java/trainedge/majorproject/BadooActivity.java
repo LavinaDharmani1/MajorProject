9d9d951aeb7152cc02a00114a1943b192e5e5260
LavinaDharmani1 <lavina.dharmani@gmail.com>
2017-05-04 14:54:45 +0530

    Pb
    few activities progress bar

--- app/src/main/java/trainedge/majorproject/BadooActivity.java
+++ app/src/main/java/trainedge/majorproject/BadooActivity.java
@@ -1,35 +1,65 @@
 package trainedge.majorproject;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class BadooActivity extends AppCompatActivity {
+
+    ProgressBar bar;
+    WebView wvBadoo;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_badoo);
+        bar =(ProgressBar) findViewById(R.id.progressBar2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        WebView wvBadoo = (WebView) findViewById(R.id.wvBadoo);
+        wvBadoo = (WebView) findViewById(R.id.wvBadoo);
         wvBadoo.getSettings().setJavaScriptEnabled(true);
         wvBadoo.setWebViewClient(new BadooCallback());
         wvBadoo.loadUrl("https://m.badoo.com");
     }
     public class BadooCallback extends WebViewClient {
+
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            return false;
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            bar.setVisibility(View.GONE);
         }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return super.shouldOverrideUrlLoading(view, url);
+        }
+    }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode==KeyEvent.KEYCODE_BACK) && wvBadoo.canGoBack()){
+            wvBadoo.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
     }
 }
--- app/src/main/java/trainedge/majorproject/GoogleActivity.java
+++ app/src/main/java/trainedge/majorproject/GoogleActivity.java
@@ -1,10 +1,12 @@
 package trainedge.majorproject;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
@@ -13,16 +15,18 @@ import android.widget.ProgressBar;
 
 public class GoogleActivity extends AppCompatActivity {
 
-
+    WebView wvGo;
+    ProgressBar bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_google);
+        bar =(ProgressBar) findViewById(R.id.progressBar2);
         Toolbar toolbar =(Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
         getSupportActionBar().setDisplayHomeAsUpEnabled(true);
 
-        WebView wvGo=(WebView) findViewById(R.id.wvGo);
+        wvGo=(WebView) findViewById(R.id.wvGo);
         wvGo.getSettings().setJavaScriptEnabled(true);
         wvGo.setWebViewClient(new GoCallback());
         wvGo.loadUrl("https://plus.google.com");
@@ -30,8 +34,31 @@ public class GoogleActivity extends AppCompatActivity {
 
     public class GoCallback extends WebViewClient {
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view,  WebResourceRequest request) {
-            return false;
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            bar.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return super.shouldOverrideUrlLoading(view, url);
         }
     }
+
+    @Override
+    public boolean onKeyDown(int keyCode, KeyEvent event) {
+        if((keyCode==KeyEvent.KEYCODE_BACK) && wvGo.canGoBack()){
+            wvGo.goBack();
+            return true;
+        }
+
+        return super.onKeyDown(keyCode, event);
+    }
 }
+
