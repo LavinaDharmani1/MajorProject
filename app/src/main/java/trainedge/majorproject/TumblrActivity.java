4ca1fa5e8bf42ed2e8102f795bd82053b4ace775
LavinaDharmani1 <lavina.dharmani@gmail.com>
2017-05-08 20:49:04 +0530

    tumblrprogress
    baractivity

--- app/src/main/java/trainedge/majorproject/TumblrActivity.java
+++ app/src/main/java/trainedge/majorproject/TumblrActivity.java
@@ -1,21 +1,27 @@
 package trainedge.majorproject;
 
+import android.graphics.Bitmap;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
+import android.view.KeyEvent;
 import android.view.View;
 import android.webkit.WebResourceRequest;
 import android.webkit.WebView;
 import android.webkit.WebViewClient;
+import android.widget.ProgressBar;
 
 public class TumblrActivity extends AppCompatActivity {
 
+    WebView wvTumblr;
+    ProgressBar bar;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_tumblr);
+        bar=(ProgressBar) findViewById(R.id.progressBar2);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
@@ -29,9 +35,34 @@ public class TumblrActivity extends AppCompatActivity {
     }
 
     private class TumblrCallback extends WebViewClient {
+
+
         @Override
-        public boolean shouldOverrideUrlLoading(WebView view, WebResourceRequest request) {
-            return false;
+        public void onPageFinished(WebView view, String url) {
+            super.onPageFinished(view, url);
+            bar.setVisibility(View.GONE);
+        }
+
+        @Override
+        public void onPageStarted(WebView view, String url, Bitmap favicon) {
+            super.onPageStarted(view, url, favicon);
+        }
+
+        @Override
+        public boolean shouldOverrideUrlLoading(WebView view, String url) {
+            view.loadUrl(url);
+            return super.shouldOverrideUrlLoading(view, url);
         }
     }
-}
+
+        @Override
+        public boolean onKeyDown(int keyCode, KeyEvent event) {
+            if((keyCode==KeyEvent.KEYCODE_BACK) && wvTumblr.canGoBack()){
+                wvTumblr.goBack();
+                return true;
+            }
+
+            return super.onKeyDown(keyCode, event);
+        }
+    }
+
